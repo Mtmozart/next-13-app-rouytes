4915df19f8777dc3ddd55d0657efede961ff6ee5
Mtmozart <mmsnborges@gmail.com>
2023-08-24 17:29:41 -0300

    generatestativ

--- src/app/(authenticated)/users/[userId]/page.tsx
+++ src/app/(authenticated)/users/[userId]/page.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { getUserById } from "../services/get-user-by-id";
 import OthersUsers from "../components/others-users";
+import { getUsers } from "../services/get-users";
+
+export async function generateStaticParams() {
+  const users = await getUsers();
+  const usersIds = users.map((user: any) => ({
+    userId: user.id.toString(),
+  }));
+  return usersIds;
+}
 export const metadata: Metadata = {
   title: "Passar como props",
   description: "Passar como props",
@@ -17,6 +26,9 @@ export default async function UserDetailPage({
         Dados: {user.first_name} {user.last_name}
       </h2>
       <span>email: {user.email}</span>
+      <hr />
+      <div>{user.body}</div>
+
       <hr />
       <h3>Outros usuários</h3>
       <OthersUsers currentUserId={params.userId} />
